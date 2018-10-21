4fc86bc827c6a37b620fc2ca42ab5f20bc61da8f
Dustin Ryan - Roepsch <dustinroepsch@gmail.com>
2018-10-20 23:15:59 -0500

    test arduino code for com

--- ardunio/serial_test.ino
+++ ardunio/serial_test.ino
@@ -2,14 +2,18 @@
 #include <HardwareSerial.h>
 // #include "../shared/CommunicationsConstants.h"
 
-void setup() {
+void setup()
+{
     Serial.begin(9600);
 }
 // int i = 0;
-void loop() {
-    if (Serial.available() > 0) {
+void loop()
+{
+    if (Serial.available() > 0)
+    {
         int bt = Serial.read();
-        Serial.write(bt + 1);
+
+        Serial.write(bt == 0 ? 8 : bt == 1 ? 34 : 10);
     }
     // i++;
 }
